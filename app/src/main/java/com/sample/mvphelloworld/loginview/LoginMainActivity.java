205b565855db9efdd75059dd2014d7ca8d776e0e
koteswara <koteswara@juniper.net>
2018-11-21 11:46:40 +0530

    gradle sdk removed

--- app/src/main/java/com/sample/mvphelloworld/loginview/LoginMainActivity.java
+++ app/src/main/java/com/sample/mvphelloworld/loginview/LoginMainActivity.java
@@ -36,6 +36,9 @@ public class LoginMainActivity extends AppCompatActivity implements LoginContrac
 
     }
 
+    public void adding(){
+
+    }
     @Override
     public void showLoginSucessMessage(String message) {
         Toast.makeText(this,message,Toast.LENGTH_SHORT).show();
